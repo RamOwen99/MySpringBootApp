7fba370006daace27129716e06e5aae01a3bccb6
OwenRamire <owenomar_ramirez@ucol.mx>
2021-02-11 12:05:50 -0600

    Initial commit

---
+++ src/main/java/com/example/MyFirstSpringApp/MyFirstSpringAppApplication.java
@@ -0,0 +1,17 @@
+package com.example.MyFirstSpringApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstSpringAppApplication {
+
+	public static void main(String[] args) {
+		/*
+		* This is the main class where Spring will execute the
+		* SpringApplication class with the run method to run the application
+		* */
+		SpringApplication.run(MyFirstSpringAppApplication.class, args);
+	}
+
+}
